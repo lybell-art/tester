f976b1dca8d8f7b8f2098ff0edaf3f83714de02a
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-12 12:45:20 +0900

    Update sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -1,19 +1,15 @@
 function setup() { 
   createCanvas(400, 400);
+	textSize(32);
 } 
 
 function draw() { 
-  f();
-	background(255);
-	console.log("ddd");
+	
 }
 
-function f()
+function touchStart()
 {
-	background(128);
-	while(true)
-	{
-		if(mouseIsPressed) return;
-		console.log("ccc");
-	}
+	background(255);
+	for(var i=0;i<touches.length;i++)
+		text(touches[i],0,i*36);
 }
