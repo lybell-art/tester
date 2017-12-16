beccbf406b47cf4dd23b7946c1849e365c46b666
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-16 15:31:39 +0900

    Create sketch7.js

---
+++ sketch7.js
@@ -0,0 +1,25 @@
+var table;
+
+function setup() {
+  table = new p5.Table();
+	var i,j;
+  for(i=0;i<10;i++)
+  {
+    table.addRow();
+		console.log("a");
+    for(j=0;j<6;j++)
+    {
+      table.set(i,j,"1");
+			console.log("a");
+    }
+  }
+	for(i=0;i<10;i++)
+  {
+    table.addRow();
+		console.log("a");
+    for(j=0;j<6;j++)
+    {
+			console.log(table.get(i,j));
+    }
+  }
+}
